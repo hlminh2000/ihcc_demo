16271ce00083de41dd47706bee5e8d3eecfebee1
Minh <hlminh2000@gmail.com>
2020-04-02 15:05:08 -0400

    💄 style update

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,17 +2,55 @@ import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import CohortRepo from "./pages/cohortRepo";
+import logo from "./logo.png";
+import { css } from "emotion";
 
 function App() {
   const customHistory = createBrowserHistory();
+  const fullView = css`
+    height: 100%;
+    max-height: 100%;
+    display: flex;
+    flex-direction: column;
+  `;
+  const headerStyle = css`
+    height: 64px;
+    display: flex;
+    align-items: center;
+    font-size: 24px;
+    font-weight: bold;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.04;
+    letter-spacing: normal;
+    color: #191970;
+    background: white;
+    border-bottom: solid 2px #dcdde1;
+    width: 100%;
+  `;
+  const logoStyle = css`
+    width: 142px;
+  `;
+  const pageContainer = css`
+    position: relative;
+    flex: 1;
+  `;
   return (
-    <Router history={customHistory}>
-      <Switch>
-        <Route exact path="/">
-          <CohortRepo />
-        </Route>
-      </Switch>
-    </Router>
+    <div className={fullView}>
+      <div className={headerStyle}>
+        <img src={logo} className={logoStyle}></img>
+        International HundredK+ Cohorts Consortium
+      </div>
+      <div className={pageContainer}>
+        <Router history={customHistory}>
+          <Switch>
+            <Route exact path="/">
+              <CohortRepo />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </div>
   );
 }
 
--- ui/src/pages/cohortRepo/index.tsx
+++ ui/src/pages/cohortRepo/index.tsx
@@ -13,44 +13,85 @@ import {
 } from "@arranger/components/dist/Arranger";
 import "@arranger/components/public/themeStyles/beagle/beagle.css";
 
-const repoStyle = css``;
+const PageContent = ({ style, ...props }: { style: {} }) => {
+  const pageContainer = css`
+    display: flex;
+    flex-direction: row;
+    max-height: 100%;
+    height: 100%;
+  `;
+  const facetPanelContainer = css`
+    max-height: calc(100vh - 64px);
+    border-right: solid 1px #dcdde1;
+    overflow: scroll;
+    .aggregation-card {
+      border-top: none;
+      .header {
+        margin: 0px;
+        .title-wrapper {
+          padding: 10px;
+          padding: 10px;
+          background-color: #e8e8f0;
+          &.collapsed {
+            padding-bottom: 10px;
+            & > .arrow {
+              padding: 0px;
+            }
+          }
+        }
+      }
+      border-left: none;
+      padding: 0px;
+      margin: 0px;
+      .filter {
+        padding-left: 5px;
+        padding-right: 5px;
+      }
+      .bucket {
+        padding: 3px 5px 5px 5px;
+      }
+    }
+  `;
+  return (
+    <div className={pageContainer}>
+      <div className={facetPanelContainer}>
+        <Aggregations
+          style={{ width: 300 }}
+          componentProps={{
+            getTermAggProps: () => ({
+              maxTerms: 3
+            })
+          }}
+          {...props}
+        />
+      </div>
+      <div
+        className={css`
+          position: relative;
+          flex-grow: 1;
+          display: flex;
+          flex-direction: column;
+          padding: 10px;
+        `}
+      >
+        <CurrentSQON {...props} />
+        <Table {...props} />
+      </div>
+    </div>
+  );
+};
 
 const CohortRepo = () => {
   const index = "cohort_centric";
   const graphqlField = "cohort";
-  const projectId = "demo_2";
+  const projectId = "demo_3";
   return (
     <Arranger
       disableSocket
       index={index}
       graphqlField={graphqlField}
       projectId={projectId}
-      render={({ style, ...props }: { style: {} }) => {
-        return (
-          <div style={{ display: "flex", ...style }} className={repoStyle}>
-            <Aggregations
-              style={{ width: 300 }}
-              componentProps={{
-                getTermAggProps: () => ({
-                  maxTerms: 3
-                })
-              }}
-              {...props}
-            />
-            <div
-              className={css`
-                position: relative;
-                flex-grow: 1;
-                display: flex;
-                flex-direction: column;
-              `}
-            >
-              <CurrentSQON {...props} />
-              <Table {...props} />
-            </div>
-          </div>
-        );
-      }}
+      render={PageContent}
     />
   );
 };
