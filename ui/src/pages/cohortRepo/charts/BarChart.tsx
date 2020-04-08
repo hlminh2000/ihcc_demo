e9bb098cd8c7857ec7d95f3aaff006d21ba77ed5
Minh <hlminh2000@gmail.com>
2020-04-08 11:27:55 -0400

    💩 tests out Sankey chart

--- ui/src/pages/cohortRepo/charts/BarChart.tsx
+++ ui/src/pages/cohortRepo/charts/BarChart.tsx
@@ -1,117 +1,163 @@
 import React from "react";
 import { ResponsiveBar } from "@nivo/bar";
+import { ResponsiveSankey } from "@nivo/sankey";
 
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 
 type ChartData = { country: string; cohorts: number }[];
 
-export default ({ sqon }: { sqon: {} | null }) => {
-  const { data, loading: loadingCountries } = useQuery<{
-    cohort: {
-      aggregations: {
-        countries: {
-          buckets: { key: string; doc_count: number }[];
-        };
-      };
-    };
-  }>(
-    gql`
-      query COUNTRIES_AGGREGATION($sqon: JSON) {
-        cohort {
-          aggregations(filters: $sqon) {
-            countries {
-              buckets {
-                key
-                doc_count
-              }
-            }
-          }
-        }
-      }
-    `,
+const data = {
+  nodes: [
     {
-      variables: {
-        sqon: sqon
-      },
-      fetchPolicy: "network-only"
+      id: "23AndMe"
+    },
+    {
+      id: "Argo"
+    },
+    {
+      id: "KF"
+    },
+    {
+      id: "GDC"
+    },
+    {
+      id: "PACA-AU"
+    },
+
+    {
+      id: "Blood"
+    },
+    {
+      id: "Stool"
+    },
+    {
+      id: "Saliva"
+    },
+    {
+      id: "Urine"
     }
-  );
-  const chartData: ChartData =
-    data?.cohort.aggregations.countries.buckets.map(({ key, doc_count }) => ({
-      cohorts: doc_count,
-      country: key
-    })) || [];
+  ],
+  links: [
+    {
+      source: "PACA-AU",
+      target: "Blood",
+      value: 1
+    },
+    {
+      source: "KF",
+      target: "Stool",
+      value: 1
+    },
+    {
+      source: "GDC",
+      target: "Stool",
+      value: 1
+    },
+    {
+      source: "23AndMe",
+      target: "Stool",
+      value: 1
+    },
+    {
+      source: "23AndMe",
+      target: "Saliva",
+      value: 1
+    },
+    {
+      source: "23AndMe",
+      target: "Urine",
+      value: 1
+    },
+    {
+      source: "Argo",
+      target: "Saliva",
+      value: 1
+    },
+    {
+      source: "Argo",
+      target: "Urine",
+      value: 1
+    }
+  ]
+};
+
+export default ({ sqon }: { sqon: {} | null }) => {
+  // const { data, loading: loadingCountries } = useQuery<{
+  //   cohort: {
+  //     aggregations: {
+  //       countries: {
+  //         buckets: { key: string; doc_count: number }[];
+  //       };
+  //     };
+  //   };
+  // }>(
+  //   gql`
+  //     query COUNTRIES_AGGREGATION($sqon: JSON) {
+  //       cohort {
+  //         aggregations(filters: $sqon) {
+  //           countries {
+  //             buckets {
+  //               key
+  //               doc_count
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `,
+  //   {
+  //     variables: {
+  //       sqon: sqon
+  //     },
+  //     fetchPolicy: "network-only"
+  //   }
+  // );
+  // const chartData: ChartData =
+  //   data?.cohort.aggregations.countries.buckets.map(({ key, doc_count }) => ({
+  //     cohorts: doc_count,
+  //     country: key
+  //   })) || [];
   return (
-    <ResponsiveBar
-      data={chartData}
-      keys={["cohorts"]}
-      indexBy="country"
-      margin={{ top: 10, right: 0, bottom: 40, left: 0 }}
-      padding={0.3}
-      colors={["#47a8bd"]}
-      enableGridY={false}
-      defs={[
-        {
-          id: "dots",
-          type: "patternDots",
-          background: "inherit",
-          color: "#38bcb2",
-          size: 4,
-          padding: 1,
-          stagger: true
-        },
-        {
-          id: "lines",
-          type: "patternLines",
-          background: "inherit",
-          color: "#eed312",
-          rotation: -45,
-          lineWidth: 6,
-          spacing: 10
-        }
-      ]}
-      borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legendPosition: "middle",
-        legendOffset: 32
-      }}
-      axisLeft={null}
-      labelSkipWidth={12}
-      labelSkipHeight={12}
-      labelTextColor={{ from: "color", modifiers: [["darker", 1.6]] }}
-      legends={[
-        {
-          dataFrom: "keys",
-          anchor: "bottom-right",
-          direction: "column",
-          justify: false,
-          translateX: 120,
-          translateY: 0,
-          itemsSpacing: 2,
-          itemWidth: 100,
-          itemHeight: 20,
-          itemDirection: "left-to-right",
-          itemOpacity: 0.85,
-          symbolSize: 20,
-          effects: [
-            {
-              on: "hover",
-              style: {
-                itemOpacity: 1
-              }
-            }
-          ]
-        }
-      ]}
-      animate={true}
-      motionStiffness={90}
-      motionDamping={15}
+    <ResponsiveSankey
+      data={data}
+      margin={{ top: 10, right: 80, bottom: 10, left: 80 }}
+      align="justify"
+      colors={{ scheme: "category10" }}
+      nodeOpacity={1}
+      nodeThickness={18}
+      nodeInnerPadding={3}
+      nodeSpacing={24}
+      nodeBorderWidth={0}
+      nodeBorderColor={{ from: "color", modifiers: [["darker", 0.8]] }}
+      linkOpacity={0.5}
+      linkHoverOthersOpacity={0.1}
+      enableLinkGradient={true}
+      labelPosition="outside"
+      labelOrientation="horizontal"
+      labelPadding={16}
+      labelTextColor={{ from: "color", modifiers: [["darker", 1]] }}
+      // legends={[
+      //   {
+      //     anchor: "bottom-right",
+      //     direction: "column",
+      //     translateX: 130,
+      //     itemWidth: 100,
+      //     itemHeight: 14,
+      //     itemDirection: "right-to-left",
+      //     itemsSpacing: 2,
+      //     itemTextColor: "#999",
+      //     symbolSize: 14,
+      //     effects: [
+      //       {
+      //         on: "hover",
+      //         style: {
+      //           itemTextColor: "#000"
+      //         }
+      //       }
+      //     ]
+      //   }
+      // ]}
     />
   );
 };
