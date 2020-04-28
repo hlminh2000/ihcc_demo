d159e8ddf3cb9eef85810b7c63b8b89ff6984a95
Minh <hlminh2000@gmail.com>
2020-04-28 14:51:15 -0400

    :sparkles: updates transformation script

---
+++ scripts/indexData.ts
@@ -0,0 +1,53 @@
+import { Client } from "@elastic/elasticsearch";
+const mapping = require("../api/assets/cohort_centric.json");
+const cohorts = require("../api/assets/transformed_data.json");
+
+const INDEX = "demo_index";
+
+const esClient = new Client({
+  node: "http://localhost:9200",
+});
+
+export const initIndexMapping = async (index: string, esClient: Client) => {
+  const serializedIndexName = index.toLowerCase();
+  await esClient.indices.putMapping({
+    index: serializedIndexName,
+    body: mapping.mappings,
+  });
+};
+
+const sleep = () =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 500);
+  });
+
+(async () => {
+  await esClient.indices
+    .delete({
+      index: INDEX,
+    })
+    .catch((err) => console.log("index already exists"))
+    .then(sleep);
+  await esClient.indices
+    .create({
+      index: INDEX,
+      body: mapping,
+    })
+    .catch((err) => console.log("create"))
+    .then(sleep);
+  await Promise.all(
+    cohorts.map((cohort: object, i: number) => {
+      try {
+        return esClient.index({
+          index: INDEX,
+          body: cohort,
+        });
+      } catch (err) {
+        console.log("i: ", i);
+        throw err;
+      }
+    })
+  );
+})();
--- scripts/transformDocs.ts
+++ scripts/transformDocs.ts
@@ -6,7 +6,12 @@ type MappingShape = {
   countries: string[];
   pi_lead?: string;
   website?: string;
-  data_types: string[];
+  available_data_types?: {
+    genomic_data: boolean;
+    environmental_data: boolean;
+    biospecimens: boolean;
+    phenotypic_clinical_data: boolean;
+  };
   basic_cohort_attributes: string[];
   biosample: {
     sample_types: string[];
@@ -29,70 +34,83 @@ type Raw = {
   countries?: string[];
   pi_lead?: string;
   website?: string;
-  datatypes?: string[];
-  "basic cohort attributes"?: string[];
-  biosample?: {
-    "sample type"?: string[];
+  available_data_types?: {
+    genomic_data: boolean;
+    environmental_data: boolean;
+    biospecimens: boolean;
+    phenotypic_clinical_data: boolean;
   };
-  "laboratory measures"?: {
+  basic_cohort_attributes?: {
+    [k: string]: string[] | null;
+  };
+  biosample?: {
+    sample_type?: string[];
+  };
+  laboratory_measures?: {
     microbiology?: string[];
   };
-  "questionnaire/survey data":
-    | string[]
-    | {
-        "lifestyle and behaviours"?: string[];
-        "physiological measurements"?: {
-          anthropometry?: string[];
-          "circulation and respiration"?: string[];
-        };
-        "socio-demographic and economic characteristics"?: string[];
-      };
+  questionnaire_survey_data: {
+    lifestyle_and_behaviours?: string[];
+    physiological_measurements?: {
+      anthropometry?: string[];
+      circulation_and_respiration?: string[];
+    };
+    socio_demographic_and_economic_characteristics?: string[];
+  };
 };
 
-const toEsDocument = (raw: Raw): MappingShape => {
-  return {
-    cohort_name: raw.cohort_name,
-    countries: raw.countries || [],
-    basic_cohort_attributes: raw["basic cohort attributes"] || [],
-    biosample: {
-      biosample_variables: [],
-      sample_types: raw.biosample?.["sample type"] || [],
-    },
-    data_types: raw.datatypes || [],
-    laboratory_measures: {
-      genomic_variables: [],
-      microbiology: raw["laboratory measures"]?.microbiology || [],
-    },
-    pi_lead: raw.pi_lead,
-    questionnaire_survey_data: Array.isArray(raw["questionnaire/survey data"])
-      ? {
-          socio_demographic_and_economic_characteristics: [],
-          lifestyle_and_behaviours: [],
-          physiological_measurements: [],
-        }
-      : {
-          socio_demographic_and_economic_characteristics:
-            raw["questionnaire/survey data"][
-              "socio-demographic and economic characteristics"
-            ] || [],
-          lifestyle_and_behaviours:
-            raw["questionnaire/survey data"]["lifestyle and behaviours"] || [],
-          physiological_measurements: [
-            ...(raw["questionnaire/survey data"]["physiological measurements"]
-              ?.anthropometry || []),
-            ...(raw["questionnaire/survey data"][
-              "physiological measurements"
-            ]?.["circulation and respiration"] || []),
-          ],
-        },
-    website: raw.website,
-  };
+const toEsDocument = (raw: Raw, i: number): MappingShape => {
+  try {
+    const output = {
+      cohort_name: raw.cohort_name,
+      countries: raw.countries || [],
+      basic_cohort_attributes: Object.values(
+        raw.basic_cohort_attributes || {}
+      ).reduce<string[]>(
+        (acc, attributes) => [...acc, ...(attributes || [])],
+        []
+      ),
+      biosample: {
+        biosample_variables: [],
+        sample_types: raw.biosample?.sample_type || [],
+      },
+      available_data_types: raw.available_data_types || {
+        biospecimens: false,
+        environmental_data: false,
+        genomic_data: false,
+        phenotypic_clinical_data: false,
+      },
+      laboratory_measures: {
+        genomic_variables: [],
+        microbiology: raw.laboratory_measures?.microbiology || [],
+      },
+      pi_lead: raw.pi_lead,
+      questionnaire_survey_data: {
+        socio_demographic_and_economic_characteristics:
+          raw.questionnaire_survey_data
+            .socio_demographic_and_economic_characteristics || [],
+        lifestyle_and_behaviours:
+          raw.questionnaire_survey_data.lifestyle_and_behaviours || [],
+        physiological_measurements: [
+          ...(raw.questionnaire_survey_data.physiological_measurements
+            ?.anthropometry || []),
+          ...(raw.questionnaire_survey_data.physiological_measurements
+            ?.circulation_and_respiration || []),
+        ],
+      },
+      website: raw.website,
+    };
+    console.log("output: ", output);
+    return output;
+  } catch (err) {
+    console.log("err: ", err);
+    throw err;
+  }
 };
 
 const raw: Raw[] = require("../api/assets/raw_data.json");
 const output_dir = "../api/assets/transformed_data.json";
 const output = raw.map(toEsDocument);
+console.log("output: ", output);
 
 fs.writeFileSync(output_dir, JSON.stringify(output, null, 2));
-
-console.log(output);
