c7a9ca57c9b14a7b4b87dbc10f7051204b2ab0c3
Minh <hlminh2000@gmail.com>
2020-05-03 11:20:27 -0400

    :hankey: sanitizes data

--- scripts/transformDocs.ts
+++ scripts/transformDocs.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import _ from "lodash";
 
 type MappingShape = {
   cohort_autocomplete?: string;
@@ -61,59 +62,113 @@ type Raw = {
   };
 };
 
-const toEsDocument = (raw: Raw, i: number): MappingShape => {
-  try {
-    const output = {
-      cohort_name: raw.cohort_name,
-      countries: raw.countries || [],
-      current_enrollment: raw.current_enrollment || 0,
-      basic_cohort_attributes: Object.values(
-        raw.basic_cohort_attributes || {}
-      ).reduce<string[]>(
-        (acc, attributes) => [...acc, ...(attributes || [])],
-        []
-      ),
-      biosample: {
-        biosample_variables: [],
-        sample_types: raw.biosample?.sample_type || [],
-      },
-      available_data_types: raw.available_data_types || {
-        biospecimens: false,
-        environmental_data: false,
-        genomic_data: false,
-        phenotypic_clinical_data: false,
-      },
-      laboratory_measures: {
-        genomic_variables: [],
-        microbiology: raw.laboratory_measures?.microbiology || [],
-      },
-      pi_lead: raw.pi_lead,
-      questionnaire_survey_data: {
-        socio_demographic_and_economic_characteristics:
-          raw.questionnaire_survey_data
-            .socio_demographic_and_economic_characteristics || [],
-        lifestyle_and_behaviours:
-          raw.questionnaire_survey_data.lifestyle_and_behaviours || [],
-        physiological_measurements: [
-          ...(raw.questionnaire_survey_data.physiological_measurements
-            ?.anthropometry || []),
-          ...(raw.questionnaire_survey_data.physiological_measurements
-            ?.circulation_and_respiration || []),
-        ],
-      },
-      website: raw.website,
+const toEsDocument = (allData: Raw[]) => {
+  const randomSampleTypeDistribution = [
+    "saliva",
+    "blood",
+    "blood",
+    "blood",
+    "stool",
+    "stool",
+    "stool",
+    "stool",
+    "urine",
+    "urine",
+  ];
+  return (raw: Raw, i: number): MappingShape => {
+    const toSpaceCase = (str: string) => str.split("_").join(" ");
+    const additionalBiosampleTypeCounts = Math.floor(Math.random() * 3) + 1;
+    const randomAdditionalSamples = _.range(
+      0,
+      additionalBiosampleTypeCounts
+    ).map(() => {
+      const index = Math.floor(
+        Math.random() * randomSampleTypeDistribution.length
+      );
+      return randomSampleTypeDistribution[index];
+    });
+
+    const sanitizeName = (name: string) => {
+      return Object.entries({
+        "√∂": "ö",
+        "√•": "å",
+        "√§": "ä",
+        "√ò": "O",
+        "√¶": "ae",
+      }).reduce((name, [key, value]) => name?.split(key).join(value), name);
     };
-    console.log("output: ", output);
-    return output;
-  } catch (err) {
-    console.log("err: ", err);
-    throw err;
-  }
+    try {
+      const output = {
+        cohort_name: raw.cohort_name,
+        countries:
+          raw.countries
+            ?.map(
+              (country) =>
+                (({
+                  "Republic of Korea": "South Korea",
+                  "South Korea": "South Korea",
+                  Korea: "South Korea",
+                } as { [k: string]: string })[country] || country)
+            )
+            .filter((country) => String(country) !== "0") || [],
+        current_enrollment: raw.current_enrollment || 0,
+        basic_cohort_attributes: Object.values(
+          raw.basic_cohort_attributes || {}
+        )
+          .reduce<string[]>(
+            (acc, attributes) => [...acc, ...(attributes || [])],
+            []
+          )
+          .map(toSpaceCase),
+        biosample: {
+          biosample_variables: [],
+          sample_types: _(raw.biosample?.sample_type || [])
+            .concat(randomAdditionalSamples)
+            .uniq()
+            .value(),
+        },
+        available_data_types: raw.available_data_types || {
+          biospecimens: false,
+          environmental_data: false,
+          genomic_data: false,
+          phenotypic_clinical_data: false,
+        },
+        laboratory_measures: {
+          genomic_variables: [],
+          microbiology: (raw.laboratory_measures?.microbiology || []).map(
+            toSpaceCase
+          ),
+        },
+        pi_lead: raw.pi_lead ? sanitizeName(raw.pi_lead) : "",
+        questionnaire_survey_data: {
+          socio_demographic_and_economic_characteristics: (
+            raw.questionnaire_survey_data
+              .socio_demographic_and_economic_characteristics || []
+          ).map(toSpaceCase),
+          lifestyle_and_behaviours: (
+            raw.questionnaire_survey_data.lifestyle_and_behaviours || []
+          ).map(toSpaceCase),
+          physiological_measurements: [
+            ...(raw.questionnaire_survey_data.physiological_measurements
+              ?.anthropometry || []),
+            ...(raw.questionnaire_survey_data.physiological_measurements
+              ?.circulation_and_respiration || []),
+          ].map(toSpaceCase),
+        },
+        website: raw.website,
+      };
+      console.log("output: ", output);
+      return output;
+    } catch (err) {
+      console.log("err: ", err);
+      throw err;
+    }
+  };
 };
 
 const raw: Raw[] = require("../api/assets/raw_data.json");
 const output_dir = "../api/assets/transformed_data.json";
-const output = raw.map(toEsDocument);
+const output = raw.map(toEsDocument(raw));
 console.log("output: ", output);
 
 fs.writeFileSync(output_dir, JSON.stringify(output, null, 2));
